e2e39c3f6b5548b9e2325196183a10920d9c0d56
Jimmy Beckett <jimmybeckett1@gmail.com>
2019-11-25 20:45:37 -0800

    Bug fix

--- vlambda.v
+++ vlambda.v
@@ -1,55 +1,53 @@
 Require Import String.
-Require Import List.
-Import ListNotations.
-Require Import Datatypes.
-Require Import Nat.
-
-(* See http://homepage.cs.uiowa.edu/~slonnegr/plf/Book/Chapter5.pdf *)
 
 Inductive Expr : Type :=
 | Var (name : string)
 | Combination (expr1 : Expr) (expr2 : Expr)
 | Abstraction (bound : string) (body : Expr).
 
-Notation "'L' x | y" := (Abstraction x y) (at level 70, right associativity).
-Notation "( x ) ( y )" := (Combination x y) (at level 80, right associativity).
-Notation "$ x" := (Var x) (at level 90, right associativity).
-
-(* Scuffed beta reduction, does not correctly distinguish between bound and free variables *)
+(* Scuffed beta reduction, does not distinguish between different variables with the same name *)
 Definition betaReduce (expr : Expr) (old : string) (new : Expr) : Expr :=
 let fix substitute (expr : Expr) : Expr :=
-    match expr with
-    | Var v => if String.eqb v old then new else expr
-    | Combination expr1 expr2 => Combination (substitute expr1) (substitute expr2)
-    | Abstraction bound body => Abstraction bound (substitute body)
-    end
+  match expr with
+  | Var v => if String.eqb v old then new else expr
+  | Combination expr1 expr2 => Combination (substitute expr1) (substitute expr2)
+  | Abstraction bound body => Abstraction bound (substitute body)
+  end
 in substitute expr.
 
 Definition eval (expr : Expr) : option Expr :=
-let fix evalOpt (expr : Expr) (n : nat) : option Expr :=
-  match expr, n with
-  | Combination e1 e2, S n' => 
-    let e1' := evalOpt e1 n' in
-    let e2' := evalOpt e2 n' in
+let fix evalAux (expr : Expr) (maxDepth : nat) : option Expr :=
+  match expr, maxDepth with
+  | Combination e1 e2, S maxDepth' => 
+    let e1' := evalAux e1 maxDepth' in
+    let e2' := evalAux e2 maxDepth' in
       match e1', e2' with
-      | Some (Abstraction bound body), Some arg  => evalOpt (betaReduce body bound arg) n'
+      | Some (Abstraction bound body), Some arg  => evalAux (betaReduce body bound arg) maxDepth'
       | Some e1'', Some e2'' => Some (Combination e1'' e2'')
       | _, _ => None
       end
-  | Abstraction bound body, S n' => 
-    match evalOpt body n' with 
+  | Abstraction bound body, S maxDepth' => 
+    match evalAux body maxDepth' with 
     | None => None 
-    | Some v => Some (Abstraction bound v)
+    | Some body' => Some (Abstraction bound body')
     end
-  | Var s, _ => Some expr
-  | _, O => None (* recursion limit reached *)
+  | Var s, S _ => Some expr
+  | _, O => None (* max recursion depth reached *)
   end
-in evalOpt expr 1024.
+in evalAux expr 4096 (* arbitrarily chosen max recursion depth *).
 
 
+(* Will write real parser later, this will do for now *)
+(* lambda x.lambda y.xy ==> L "x" | L "y" | (($"x") ($"y")) *)
+Notation "'L' x | y" := (Abstraction x y) (at level 70, right associativity).
+Notation "( x ) ( y )" := (Combination x y) (at level 80).
+Notation "$ x" := (Var x) (at level 90).
+
+
+(* A simple example, using booleans *)
+(* Definitions all use different variables to avoid breaking scuffed beta reduction *)
 Definition T := L "a" | L "b" | $"a".
-Definition F := L "a" | L "b" | $"b".
-
+Definition F := L "c" | L "d" | $"d". 
 Definition and :=  L "x" | L "y" | ((($"x") ($"y")) (F)).
 
 Compute eval (((and) (T)) (T)). (* T *)
