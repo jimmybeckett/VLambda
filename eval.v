3d1a8dadb60f6c9b9ecd858d88a24ba8cb7905e7
Jimmy Beckett <jimmybeckett1@gmail.com>
2019-11-26 14:29:57 -0800

    Pretty print for parser

--- eval.v
+++ eval.v
@@ -7,34 +7,34 @@ Inductive Expr : Type :=
 
 (* Scuffed beta reduction, does not distinguish between different variables with the same name *)
 Definition betaReduce (expr : Expr) (old : string) (new : Expr) : Expr :=
-let fix substitute (expr : Expr) : Expr :=
-  match expr with
-  | Var v => if String.eqb v old then new else expr
-  | Combination expr1 expr2 => Combination (substitute expr1) (substitute expr2)
-  | Abstraction bound body => Abstraction bound (substitute body)
-  end
-in substitute expr.
+  let fix substitute (expr : Expr) : Expr :=
+      match expr with
+      | Var v => if eqb v old then new else expr
+      | Combination expr1 expr2 => Combination (substitute expr1) (substitute expr2)
+      | Abstraction bound body => Abstraction bound (substitute body)
+      end
+  in substitute expr.
 
 Definition eval (expr : Expr) : option Expr :=
-let fix evalAux (expr : Expr) (maxDepth : nat) : option Expr :=
-  match expr, maxDepth with
-  | Combination e1 e2, S maxDepth' => 
-    let e1' := evalAux e1 maxDepth' in
-    let e2' := evalAux e2 maxDepth' in
-      match e1', e2' with
-      | Some (Abstraction bound body), Some arg  => evalAux (betaReduce body bound arg) maxDepth'
-      | Some e1'', Some e2'' => Some (Combination e1'' e2'')
-      | _, _ => None
+  let fix evalAux (expr : Expr) (maxDepth : nat) : option Expr :=
+      match expr, maxDepth with
+      | Combination e1 e2, S maxDepth' => 
+        let e1' := evalAux e1 maxDepth' in
+        let e2' := evalAux e2 maxDepth' in
+          match e1', e2' with
+          | Some (Abstraction bound body), Some arg  => evalAux (betaReduce body bound arg) maxDepth'
+          | Some e1'', Some e2'' => Some (Combination e1'' e2'')
+          | _, _ => None
+          end
+      | Abstraction bound body, S maxDepth' => 
+        match evalAux body maxDepth' with 
+        | None => None 
+        | Some body' => Some (Abstraction bound body')
+        end
+      | Var _, S _ => Some expr
+      | _, O => None (* max recursion depth reached *)
       end
-  | Abstraction bound body, S maxDepth' => 
-    match evalAux body maxDepth' with 
-    | None => None 
-    | Some body' => Some (Abstraction bound body')
-    end
-  | Var _, S _ => Some expr
-  | _, O => None (* max recursion depth reached *)
-  end
-in evalAux expr 4096 (* arbitrarily chosen max recursion depth *).
+  in evalAux expr 4096 (* arbitrarily chosen max recursion depth *).
 
 
 (* Will write real parser later, this will do for now *)
---
+++ parser.v
@@ -0,0 +1,13 @@
+Require Import String.
+Add LoadPath "~/cse/VLambda".
+Load eval.
+
+Fixpoint prettyPrint (expr : Expr) : string :=
+match expr with
+| Var s => s
+| Combination e1 e2 => "(" ++ (prettyPrint e1) ++ ")" ++ "(" ++ (prettyPrint e2) ++ ")"
+| Abstraction bound body => "L " ++ bound ++ "." ++ (prettyPrint body) 
+end.
+
+Compute prettyPrint ((L "a" | L "b" | (($"a")($"b"))) ($"z")).
+
